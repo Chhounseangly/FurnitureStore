99aefcefd382ce8f69a47e09e6ee5ef21b2b3efa
មុនី​ រតន៍​ <dr.munyroth@gmail.com>
2023-11-11 16:53:54 +0700

    update on ProductListViewModel

--- app/src/main/java/kh/edu/rupp/ite/furniturestore/viewmodel/ProductListViewModel.kt
+++ app/src/main/java/kh/edu/rupp/ite/furniturestore/viewmodel/ProductListViewModel.kt
@@ -3,34 +3,35 @@ package kh.edu.rupp.ite.furniturestore.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kh.edu.rupp.ite.furniturestore.model.api.model.ApIData
 import kh.edu.rupp.ite.furniturestore.model.api.model.Product
-import kh.edu.rupp.ite.furniturestore.model.api.model.Res
 import kh.edu.rupp.ite.furniturestore.model.api.service.RetrofitInstance
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ProductListViewModel: ViewModel() {
     private val _productsData = MutableLiveData<ApIData<List<Product>>>()
     val productsData: LiveData<ApIData<List<Product>>>
         get() = _productsData
-    private lateinit var product: List<Product>
 
     fun loadProductsData(){
-        RetrofitInstance.get().api.loadProductList().enqueue(object : Callback<Res<Product>> {
-            override fun onResponse(call: Call<Res<Product>>, response: Response<Res<Product>>) {
-                val responseData = response.body()
-                if (responseData != null) {
-                    val apiData = ApIData(response.code(), responseData.data)
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response = RetrofitInstance.get().api.loadProductList()
+                if (response != null) {
+                    val apiData = ApIData(200, response.data)
                     _productsData.postValue(apiData)
-                } else {
-                    println("Response data is null")
                 }
+                else println("Response data is null")
+            } catch (ex: Exception) {
+                println("Failure: ${ex.message}")
             }
-            override fun onFailure(call: Call<Res<Product>>, t: Throwable) {
-                println("Failure: ${t.message}")
+
+            withContext(Dispatchers.Main.immediate) {
+
             }
-        })
+        }
     }
 }
