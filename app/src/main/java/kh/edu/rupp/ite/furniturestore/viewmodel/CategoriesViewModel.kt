9b228387d7ddaf6d6c50e256197acf6e31d71170
Chhounseangly <91061974+Chhounseangly@users.noreply.github.com>
2023-11-13 17:01:39 +0700

    add laoding of fetching categoryTypes in file HomeFragment and disable logo andriod when running app

--- app/src/main/java/kh/edu/rupp/ite/furniturestore/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/kh/edu/rupp/ite/furniturestore/viewmodel/CategoriesViewModel.kt
@@ -3,12 +3,17 @@ package kh.edu.rupp.ite.furniturestore.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kh.edu.rupp.ite.furniturestore.model.api.model.ApIData
 import kh.edu.rupp.ite.furniturestore.model.api.model.ApiResponse
 import kh.edu.rupp.ite.furniturestore.model.api.model.CategoryModel
 import kh.edu.rupp.ite.furniturestore.model.api.model.CategoryTypes
 import kh.edu.rupp.ite.furniturestore.model.api.model.Data
+import kh.edu.rupp.ite.furniturestore.model.api.model.Product
 import kh.edu.rupp.ite.furniturestore.model.api.service.RetrofitInstance
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,21 +31,36 @@ class CategoriesViewModel : ViewModel() {
 
 
     fun loadCategoryTypes() {
-        RetrofitInstance.get().api.loadCategories().enqueue(object : Callback<CategoryModel> {
-            override fun onResponse(call: Call<CategoryModel>, response: Response<CategoryModel>) {
-                val responseData = response.body()
+        var apiData = ApIData<List<CategoryTypes>>(102,null) //status 102 is processing
+        _categoryTypesData.postValue(apiData)
+        viewModelScope.launch(Dispatchers.IO) {
+           apiData =  try {
+               val response = RetrofitInstance.get().api.loadCategories()
+               ApIData(200, response.data)
+            }catch (ex: Exception){
+                ApIData(204, null)
+            }
 
-                if (responseData != null) {
-                    val apiData = ApIData(response.code(), responseData.data)
-                    _categoryTypesData.postValue(apiData)
-                } else {
-                    println("Response data is null")
-                }
+            withContext(Dispatchers.Main.immediate){
+                _categoryTypesData.postValue(apiData)
             }
-            override fun onFailure(call: Call<CategoryModel>, t: Throwable) {
-                println("Failure: ${t.message}")
-            }
-        })
+
+        }
+//        RetrofitInstance.get().api.loadCategories().enqueue(object : Callback<CategoryModel> {
+//            override fun onResponse(call: Call<CategoryModel>, response: Response<CategoryModel>) {
+//                val responseData = response.body()
+//
+//                if (responseData != null) {
+//                    val apiData = ApIData(response.code(), responseData.data)
+//                    _categoryTypesData.postValue(apiData)
+//                } else {
+//                    println("Response data is null")
+//                }
+//            }
+//            override fun onFailure(call: Call<CategoryModel>, t: Throwable) {
+//                println("Failure: ${t.message}")
+//            }
+//        })
     }
 
 
