91842cb7ed7343cb466c26f2f8cdebda95d8207c
Chhounseangly <91061974+Chhounseangly@users.noreply.github.com>
2023-11-24 15:30:04 +0700

    add some feather for search

--- app/src/main/java/kh/edu/rupp/ite/furniturestore/view/fragments/SearchFragment.kt
+++ app/src/main/java/kh/edu/rupp/ite/furniturestore/view/fragments/SearchFragment.kt
@@ -3,7 +3,6 @@ package kh.edu.rupp.ite.furniturestore.view.fragments
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -37,37 +36,49 @@ class SearchFragment() : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val searchView = fragmentSearchBinding.searchProduct
-        val textview = fragmentSearchBinding.notFound
-        textview.text = "No Data"
-        textview.visibility = View.INVISIBLE
-        searchView.clearFocus()
+        val noData = fragmentSearchBinding.notFound
+        val loading = fragmentSearchBinding.loading
 
         searchView.setOnQueryTextListener(object :
             androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                if (query != null) {
-                    searchViewHolder.search(query)
-                    searchViewHolder.data.observe(viewLifecycleOwner) {
-                        when (it.status) {
-                            Status.Success -> it.data?.let { it1 ->
-                                displayProductSearchFound(it1)
-                                fragmentSearchBinding.notFound.visibility = View.INVISIBLE
-                            }
-                            else -> {
-                                displayProductSearchFound(null)
-                                fragmentSearchBinding.notFound.visibility = View.VISIBLE
-                            }
-                        }
-                    }
-                }
                 return true
             }
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                if (newText != null) {
-                    fragmentSearchBinding.notFound.visibility = View.INVISIBLE
-                    displayProductSearchFound(null)
-                }
+            override fun onQueryTextChange(query: String?): Boolean {
+                fragmentSearchBinding.notFound.visibility = View.GONE
+
+                // Use a delay before triggering the search
+                handler.removeCallbacksAndMessages(null)
+                handler.postDelayed({
+                    // Trigger your search operation with newText
+                    if (query != null) {
+                        searchViewHolder.search(query)
+                        searchViewHolder.data.observe(viewLifecycleOwner) {
+                            when (it.status) {
+                                Status.Processing -> {
+                                    loading.visibility = View.VISIBLE
+                                    noData.visibility = View.GONE
+                                    displayProductSearchFound(null)
+                                }
+                                Status.Success -> it.data?.let { it1 ->
+                                    loading.visibility = View.GONE
+                                    noData.visibility = View.GONE
+                                    displayProductSearchFound(it1)
+                                }
+                                Status.Failed -> {
+                                    loading.visibility = View.GONE
+                                    noData.visibility = View.VISIBLE
+                                    displayProductSearchFound(null)
+                                }
+                            }
+                        }
+                    }else {
+                        displayProductSearchFound(null)
+                        noData.visibility = View.GONE
+                    }
+                }, 500) // 500 milliseconds (0.5 seconds) delay
+
                 return true
             }
         })
