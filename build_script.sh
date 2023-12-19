efe56fb06763df0d266a9b4b5703b4ca97dd6f2d
មុនី​ រតន៍​ <dr.munyroth@gmail.com>
2023-12-19 19:54:56 +0700

    - implement CI/CI v1.12.1

--- build_script.sh
+++ build_script.sh
@@ -9,8 +9,12 @@ else
   flavor='prd'
 fi
 
-./gradlew "assemble$flavor$build_type"
-apkDir="app/build/outputs/apk/$flavor/$build_type"
-apkMetaFile="$apkDir/output-metadata.json"
-apkFile=$(cat $apkMetaFile | jq -r '.elements[0].outputFile')
-cp "$apkDir/$apkFile" "furniture-store.apk"
+echo "tag: $tag"
+echo "build_type: $build_type"
+echo "flavor: $flavor"
+
+#./gradlew "assemble$flavor$build_type"
+#apkDir="app/build/outputs/apk/$flavor/$build_type"
+#apkMetaFile="$apkDir/output-metadata.json"
+#apkFile=$(cat $apkMetaFile | jq -r '.elements[0].outputFile')
+#cp "$apkDir/$apkFile" "furniture-store.apk"
